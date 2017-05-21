5d1dbd2cfcd05afd807de787088307417660dc41
Nathan Larson <nathnz@hotmail.co.nz>
2017-05-22 01:05:11 +1200

    Merged FinishedLevel script into RestartLevel script
    - Merged them as they do similar things. Still need to delete
    FinishLevel.
    - Started working on Main Menu features.
    - Moved all scripts into a script folder so its easier to transfer
    scripts from unity to repo.

---
+++ Scripts/MainMenu/GameOptions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOptions : MonoBehaviour {
+
+    //UI Components
+    public Dropdown resolutionDropdown;
+
+    //Screen Resolution Variables
+    private bool fullscreen;
+    private int[] width;
+    private int[] height;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    public void changeFullScreen()
+    {
+        fullscreen = !fullscreen;
+    }
+
+    public void setResolution()
+    {
+        int resolution = resolutionDropdown.value;
+        Screen.SetResolution(width[resolution], height[resolution], fullscreen);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Scripts/RestartLevel.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartLevel : MonoBehaviour {
+
+    public HighScorePanel highScorePanel;
+
+    private PlayerMovement player;
+    private Vector3 startPos;
+    
+    // Use this for initialization
+    void Start ()
+    {
+        player = GameObject.Find("Player").GetComponent<PlayerMovement>();
+        startPos = player.transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Player has crossed finish line
+        if (other.tag == "Player")
+        {
+            other.GetComponent<PlayerMovement>().setRunning(false);
+            levelFinished();
+        }
+    }
+
+    void restartLevel()
+    {
+        //Reset player 
+        player.transform.position = startPos;
+        player.StopAllCoroutines();
+        player.resetVelocity();
+
+        //Reset Timer
+        Timer.instance.resetTimer();
+        Timer.instance.setTimerRunning(true);
+
+        //Close highscore board
+        highScorePanel.gameObject.SetActive(false);
+    }
+
+    void levelFinished()
+    {
+        Timer.instance.setTimerRunning(false);
+        highScorePanel.gameObject.SetActive(true);
+        highScorePanel.StartCoroutine("setupHighScorePanel", Timer.instance.getTime());
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            restartLevel();
+        }
+    }
+}
